8d6690161e43d349006c81d731471d19cfbd1e56
Travis Illig <travis.illig@silverlining.ventures>
2017-02-09 10:46:06 -0800

    CallbackContainer for ability to change callbacks during registration.

---
+++ test/Autofac.Test/Builder/CallbackContainerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Builder;
+using Xunit;
+
+namespace Autofac.Test.Builder
+{
+    public class CallbackContainerTests
+    {
+        [Fact]
+        public void Callback_Null()
+        {
+            var c = new CallbackContainer(reg => { });
+            Assert.Throws<ArgumentNullException>(() => { c.Callback = null; });
+        }
+
+        [Fact]
+        public void Ctor_NullCallback()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CallbackContainer(null));
+        }
+
+        [Fact]
+        public void Ctor_SetsId()
+        {
+            var c = new CallbackContainer(reg => { });
+            Assert.NotEqual(Guid.Empty, c.Id);
+        }
+    }
+}
