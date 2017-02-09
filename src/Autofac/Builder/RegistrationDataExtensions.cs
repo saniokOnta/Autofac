aa5b8ba091b741711e650d458e5a3840d21b138f
Travis Illig <travis.illig@silverlining.ventures>
2017-02-09 11:35:12 -0800

    Added storage and methods for attaching a callback container to a registration builder.

---
+++ src/Autofac/Builder/RegistrationDataExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Autofac.Builder
+{
+    /// <summary>
+    /// Extension methods for <see cref="RegistrationData"/>.
+    /// </summary>
+    public static class RegistrationDataExtensions
+    {
+        private static readonly string CallbackContainerKey = "__Autofac$CallbackContainer";
+
+        /// <summary>
+        /// Gets the callback container from registration metadata.
+        /// </summary>
+        /// <param name="data">The data to search for the container.</param>
+        /// <returns>
+        /// A <see cref="CallbackContainer"/> if one is set; or <see langword="null" /> if
+        /// none is found.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="data" /> is <see langword="null" />.
+        /// </exception>
+        public static CallbackContainer GetCallbackContainer(this RegistrationData data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            var container = (object)null;
+            if (data.Metadata.TryGetValue(CallbackContainerKey, out container))
+            {
+                return container as CallbackContainer;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the callback container in registration metadata.
+        /// </summary>
+        /// <param name="data">The data to in which the container should be stored.</param>
+        /// <param name="container">
+        /// The <see cref="CallbackContainer"/> with the reference to the callback
+        /// associated with the registration builder. If this is <see langword="null" />,
+        /// the callback will be removed from the registration data.
+        /// </param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if <paramref name="data" /> is <see langword="null" />.
+        /// </exception>
+        public static void SetCallbackContainer(this RegistrationData data, CallbackContainer container)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (container == null)
+            {
+                data.Metadata.Remove(CallbackContainerKey);
+            }
+            else
+            {
+                data.Metadata[CallbackContainerKey] = container;
+            }
+        }
+    }
+}
---
+++ test/Autofac.Test/Builder/RegistrationBuilderExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Autofac.Builder;
+using Xunit;
+
+namespace Autofac.Test.Builder
+{
+    public class RegistrationBuilderExtensionsTests
+    {
+        [Fact]
+        public void GetCallbackContainer_GetsContainer()
+        {
+            var rb = RegistrationBuilder.ForType(typeof(object));
+            var c = new CallbackContainer(reg => { });
+            rb.SetCallbackContainer(c);
+            Assert.Same(c, rb.GetCallbackContainer());
+        }
+
+        [Fact]
+        public void GetCallbackContainer_NoneSet()
+        {
+            var rb = RegistrationBuilder.ForType(typeof(object));
+            Assert.Null(rb.GetCallbackContainer());
+        }
+
+        [Fact]
+        public void GetCallbackContainer_NullData()
+        {
+            Assert.Throws<ArgumentNullException>(() => RegistrationBuilderExtensions.GetCallbackContainer<object, ConcreteReflectionActivatorData, SingleRegistrationStyle>(null));
+        }
+
+        [Fact]
+        public void SetCallbackContainer_NullContainerAllowed()
+        {
+            var rb = RegistrationBuilder.ForType(typeof(object));
+            rb.SetCallbackContainer(null);
+            Assert.Null(rb.GetCallbackContainer());
+        }
+
+        [Fact]
+        public void SetCallbackContainer_NullData()
+        {
+            var c = new CallbackContainer(reg => { });
+            Assert.Throws<ArgumentNullException>(() => RegistrationBuilderExtensions.SetCallbackContainer<object, ConcreteReflectionActivatorData, SingleRegistrationStyle>(null, c));
+        }
+
+        [Fact]
+        public void SetCallbackContainer_NullRemovesContainer()
+        {
+            var rb = RegistrationBuilder.ForType(typeof(object));
+            var c = new CallbackContainer(reg => { });
+            rb.SetCallbackContainer(c);
+            rb.SetCallbackContainer(null);
+            Assert.Null(rb.GetCallbackContainer());
+        }
+    }
+}
